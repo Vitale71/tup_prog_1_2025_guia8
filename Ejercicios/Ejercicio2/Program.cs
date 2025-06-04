9cd1f6a7967921d16a3b3daf0a63196e632acb1e
Vitale71 <lucasvitale71@gmail.com>
2025-06-04 18:51:19 -0300

    .

---
+++ Ejercicios/Ejercicio2/Program.cs
@@ -0,0 +1,168 @@
+﻿namespace Ejercicio2
+{
+    internal class Program
+    {
+        #region resolucion del problema
+        static int edad1;
+        static int edad2;
+        static int edad3;
+        static int edad4;
+        static double monto;
+        static double porcentaje1;
+        static double porcentaje2;
+        static double porcentaje3;
+        static double porcentaje4;
+        static double monto1;
+        static double monto2;
+        static double monto3;
+        static double monto4;
+        #endregion
+
+        #region metodos de calculos
+        static double RegistrarMontoArepartir(double montoRegistrado)
+        { 
+            monto = montoRegistrado;
+            return monto;
+        }
+
+        static int RegistrarEdad(int edadNiña, int nroNiña)
+        {
+            if (nroNiña == 1)
+            {
+                edad1 = edadNiña;
+            }
+            if (nroNiña == 2)
+            {
+                edad2 = edadNiña;
+            }
+            if (nroNiña == 3)
+            {
+                edad3 = edadNiña;
+            }
+            if (nroNiña == 4)
+            {
+                edad4 = edadNiña;
+            }
+            return edadNiña;
+        }
+        static void CalcularMontosYporcentajesARepartir()
+        {
+            
+            int edadTotal = edad1 + edad2 + edad3 + edad4;
+            porcentaje1 = (edad1 * 100) / edadTotal;
+            porcentaje2 = (edad2 * 100) / edadTotal;
+            porcentaje3 = (edad3 * 100) / edadTotal;
+            porcentaje4 = (edad4 * 100) / edadTotal;
+            monto1 = (porcentaje1 * monto) / 100;
+            monto2 = (porcentaje2 * monto) / 100;
+            monto3 = (porcentaje3 * monto) / 100;
+            monto4 = (porcentaje4 * monto) / 100;
+        }
+        #endregion
+
+        #region metodos de pantalla
+        static int MostrarPantallaSolicitarOpcionesMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\tEjercicio1\n\n");
+            Console.WriteLine("1 - Iniciar Monto a repartir.");
+            Console.WriteLine("2 - Solicitar Edad Por niña.");
+            Console.WriteLine("3 - Mostrar monto y porcentajes que corresponde a cada niña.");
+            int op = Convert.ToInt32(Console.ReadLine());
+            return op;
+        }
+        static void MostrarPantallaSolicitarMontoARepartir()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t Registrar monto a repartir. \n\n");
+            Console.WriteLine("ingrese el monto a repartir\n\n");
+            int valor = Convert.ToInt32(Console.ReadLine());
+            RegistrarMontoArepartir(valor);
+        }
+        static void MostrarPantallaSolicitarEdadesNiñas()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t Solicitar edad por niña.\n\n");
+            Console.WriteLine("ingrese la niña numero (1-4)");
+            int niña = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("\t\t Solicitar edad por niña.\n\n");
+            Console.WriteLine("ingrese la edad de la niña numero {0}" , niña);
+            int edad = Convert.ToInt32(Console.ReadLine());
+            if (niña >= 1 && niña <= 4 && edad >= 1)
+            {
+                RegistrarEdad(edad, niña);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("\t\t Solicitar edad por niña.\n\n");
+                Console.WriteLine("edad de la niña no registrada numeros no validos. \n\n");
+                Console.WriteLine("presione enter para seguir");
+                Console.ReadKey();
+            }
+        }
+
+        static void MostrarPantallaCalcularMostrarMontoARepartir()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t Mostrar monto y porcentajes que corresponde a cada niña. \n\n");
+            if (monto != 0)
+            {
+                if (edad1 != 0 && edad2 != 0 && edad3 != 0 && edad4 != 0)
+                {
+                    CalcularMontosYporcentajesARepartir();
+                    Console.WriteLine("niña edad: {0} el porcentaje es {1} el monto correspondiente: {2}", edad1, porcentaje1, monto1);
+                    Console.WriteLine("niña edad: {0} el porcentaje es {1} el monto correspondiente: {2}", edad2, porcentaje2, monto2);
+                    Console.WriteLine("niña edad: {0} el porcentaje es {1} el monto correspondiente: {2}", edad3, porcentaje3, monto3);
+                    Console.WriteLine("niña edad: {0} el porcentaje es {1} el monto correspondiente: {2}\n\n", edad4, porcentaje4, monto4);
+                    Console.WriteLine("presione enter para seguir");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("faltan edades que ingresar para mostrar los datos\n\n");
+                    Console.WriteLine("presione enter para seguir");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine("falta el monto a repartir para mostrar los datos\n\n");
+                Console.WriteLine("presione enter para seguir");
+                Console.ReadKey();
+            }
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            int op = MostrarPantallaSolicitarOpcionesMenu();
+            while (op != -1)
+            {
+                switch (op)
+                {
+                    case 1:
+                        MostrarPantallaSolicitarMontoARepartir();
+                        break;
+
+                    case 2:
+                        MostrarPantallaSolicitarEdadesNiñas();
+                        break;
+
+                    case 3:
+                        MostrarPantallaCalcularMostrarMontoARepartir();
+                        break;
+                    default:
+                        op = -1;
+                        break;
+                }
+                if (op != -1)
+                {
+                    op = MostrarPantallaSolicitarOpcionesMenu();
+                }
+            }
+        }
+
+    }
+}
---
+++ Ejercicios/ejercicio1/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        #region resolucion del problema
+        static int acumulador;
+        static int contador;
+        static int maximo;
+        static int minimo;
+        #endregion
+
+        #region metodos de pantalla
+        static void inicializarVariables()
+        {
+            acumulador = 0;
+            contador = 0;
+            maximo = 0;
+            minimo = 0;
+            Console.Clear();
+            Console.WriteLine("\t\t Reiniciar variables. \n\n");
+            Console.WriteLine("se reiniciaron las variables correctamente\n\n");
+            Console.WriteLine("presione enter para seguir");
+            Console.ReadKey();
+        }
+
+        static void registrarValor(int valor)
+        {
+            acumulador += valor;
+
+
+            if (valor > maximo || contador == 0)
+            {
+                maximo = valor;
+            }
+
+            if (valor < minimo || contador == 0)
+            {
+                minimo = valor;
+            }
+            contador += 1;
+        }
+        static double CalcularPromedio()
+        {
+            double prom = 0;
+            if (contador != 0)
+            {
+                prom = 1.0 * acumulador / contador;
+            }
+            return prom;
+        }
+        static void MostrarMaximoyMinimo()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t mostrar maximo y minimo. \n\n");
+            if (contador != 0)
+            {
+                Console.WriteLine($"Maximo: {maximo}");
+                Console.WriteLine($"Minimo: {minimo}\n\n\n");
+            }
+            else
+            {
+                Console.WriteLine("no hubo ingresos todavia\n\n");
+            }
+            Console.WriteLine("presione enter para seguir");
+            Console.ReadKey();
+        }
+        static void MostrarPromedio()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t Mostrar promedio \n\n");
+            double prom = CalcularPromedio();
+            if (prom != 0)
+            {
+                Console.WriteLine($"el promedio es: {prom}\n\n\n");
+            }
+            else
+            {
+                Console.WriteLine("no hubo ingresos todavia\n\n");
+            }
+            Console.WriteLine("presione enter para seguir");
+            Console.ReadKey();
+        }
+        static void MostrarPantallaCantidad()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t Mostrar cantidad de numeros \n\n\n");
+
+            Console.WriteLine($"la cantidad de numeros ingresados es: {contador}\n\n\n");
+
+            Console.WriteLine("presione enter para seguir");
+            Console.ReadKey();
+        }
+        #endregion
+
+        #region metodos de pantalla
+        static int MostrarPantallaSolicitarOpcionesMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\tEjercicio1\n\n");
+            Console.WriteLine("1. - procesar un solo numero.");
+            Console.WriteLine("2. - procesar varios numeros.");
+            Console.WriteLine("3. - mostrar maximo y minimo.");
+            Console.WriteLine("4. - Mostrar promedio.");
+            Console.WriteLine("5. - mostrar cantidad de numeros ingresados.");
+            Console.WriteLine("6. - reiniciar variables.");
+            int op = Convert.ToInt32(Console.ReadLine());
+            return op;
+        }
+        static void SolicitarPantallaIngresarNumero()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t solicitud de valor a el usuario\n\n");
+            Console.WriteLine("ingrese un numero");
+            int valor = Convert.ToInt32(Console.ReadLine());
+            registrarValor(valor);
+        }
+        static void SolicitarPantallaIngresarVariosNumero()
+        {
+                Console.Clear();
+                Console.WriteLine("\t\t Procesar varios valores a el usuario\n\n");
+                Console.WriteLine("ingrese numeros o escriba -1 para salir");
+                int valor = Convert.ToInt32(Console.ReadLine());
+                while (valor != -1) {
+                if (valor != -1)
+                {
+                    registrarValor(valor);
+                }
+                Console.Clear();
+                Console.WriteLine("\t\t Procesar varios valores a el usuario\n\n");
+                Console.WriteLine("ingrese numeros o escriba -1 para salir");
+                valor = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            int op = MostrarPantallaSolicitarOpcionesMenu();
+            while (op != -1)
+            {
+                switch (op)
+                {
+                    case 1:
+                        SolicitarPantallaIngresarNumero();
+                        break;
+
+                    case 2:
+                        SolicitarPantallaIngresarVariosNumero();
+                        break;
+
+                    case 3:
+                        MostrarMaximoyMinimo();
+                        break;
+
+                    case 4:
+                        MostrarPromedio();
+                        break;
+                    case 5:
+                        MostrarPantallaCantidad();
+                        break;
+                    case 6:
+                        inicializarVariables();
+                        break;
+
+                    default:
+                        op = -1;
+                        break;
+                }
+                if (op != -1)
+                {
+                    op = MostrarPantallaSolicitarOpcionesMenu();
+                }
+            }
+        }
+
+    }
+}
